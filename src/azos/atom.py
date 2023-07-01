3135fd8d16a3e08715c0d4b335f7bf3287c9ee82
Dmitriy Khmaladze <d@zhabis.com>
2023-07-01 16:35:18 -0400

    #1 Add repo structure

---
+++ src/azos/atom.py
@@ -0,0 +1,27 @@
+# Atom
+"""Provides interop functions for working with Azos Atom data type
+
+
+"""
+
+__atoms = {
+    "key": 123
+}
+
+def encode(str):
+    """Encodes a string value as an atom ulong
+    
+    Documentation paragraph.
+    """
+    
+    print("Encoding: " + str)
+
+def decode(int):
+    """Decodes an ulong int atom value back to string
+    
+    Documentation paragraph.
+    """
+
+    print("Decoding: " + int)
+
+
