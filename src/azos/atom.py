787edb20732da9cc1a527953e9ec64342e67cf8d
Dmitriy Khmaladze <d@zhabis.com>
2023-07-02 15:26:07 -0400

    #1 Add atom decode logic

--- src/azos/atom.py
+++ src/azos/atom.py
@@ -1,27 +1,76 @@
-# Atom
 """Provides interop functions for working with Azos Atom data type
+
+Copyright (C) 20023 Azist, MIT License
 
 
 """
 
-__atoms = {
-    "key": 123
-}
+from exceptions import *
 
-def encode(str):
+MAX_ATOM_LENGTH = 8
+VALID_CHAR_CODES = set(
+                        list(range(ord('0'), ord('9') + 1)) + 
+                        [ord('_'), ord('-')] +
+                        list(range(ord('A'), ord('Z') + 1)) + 
+                        list(range(ord('a'), ord('z') + 1)) 
+                   ) #set
+
+
+__atoms = { }
+
+def __validate_char(c: int) -> bool:
+    return (c in VALID_CHAR_CODES)
+
+def encode(sval: str):
     """Encodes a string value as an atom ulong
     
     Documentation paragraph.
     """
     
     print("Encoding: " + str)
+    return 0
 
-def decode(int):
+def decode(id: int) -> str:
     """Decodes an ulong int atom value back to string
     
-    Documentation paragraph.
+    The function maintains internal static cache. If the value is
+    not in cache it gets converted to string and cached.
+
+    !!!Warning!!!: atoms are designed to handle a finite number of values,
+    most applications dealing with a few hundred values.
+    Using atoms for arbitrary values is not a good idea as it trashes your
+    Atom intern pool (cache).
     """
+    if id == 0:
+        return ""
 
-    print("Decoding: " + int)
+    result = __atoms.get(id) #check cache
+    ax = id
+    if result == None:
+        result = ""
+        ### print(f"Atom {id} is not in cache")
+        for i in range(0, 8):
+            c = ax & 0xff
+            if c==0: 
+                break
+            if not __validate_char(c):
+                raise AzosError(f"Invalid atom char #{c} / `{chr(c)}`", "atom", "decode({id})")
+            result = result + chr(c)
+            ax = ax >> 8
+
+    __atoms[id] = result
+    return result
 
 
+
+if __name__ == "__main__":
+   # for one in VALID_CHAR_CODES:
+   #    print(f"{one} - {chr(one)}")
+    print(f"Decoding atom from int: {decode(1634560356)}") # dima
+    print(f"Decoding atom from int: {decode(1634560358)}") # fima
+    print(f"Decoding atom from int: {decode(1634560356)}") # dima
+    print(f"Decoding atom from int: {decode(1634560356)}") # dima
+    print(f"Decoding atom from int: {decode(1634560356)}") # dima
+    print(f"Decoding atom from int: {decode(1634560356)}") # dima
+    print(f"Decoding atom from int: {decode(7956003944985229683 )}") # syslogin
+    print(f"Decoding atom from int: {decode(1634560358)}") # fima
---
+++ src/azos/exceptions.py
@@ -0,0 +1,27 @@
+"""Provides custom exception related to Azos interop
+
+Copyright (C) 20023 Azist, MIT License
+
+
+"""
+
+__all__ = ["AzosError"]
+
+class AzosError(Exception):
+    """Exception thrown by Azos interop code
+
+    Attributes:
+        topic -- exception topic string 
+        frm -- string specifier of the "from" place in code/system/component
+        src -- error/source code (int)
+        message -- explanation of the error
+    """
+
+    def __init__(self, message="Azos unspecified error", topic = "azos", frm = "", src = 0):
+        self.topic = topic
+        self.frm = frm
+        self.src = src
+        self.message = message
+        super().__init__(self.message)
+
+
