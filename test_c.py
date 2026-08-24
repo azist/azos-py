886be5e7b9501c9d6bcf76e352962b2f875ed1b1
DKh <d@zhabis.com>
2026-08-23 22:23:33 -0400

    Add PgConnector

--- test_c.py
+++
@@ -1,9 +0,0 @@
-
-import asyncpg
-import asyncio
-
-async def run():
-    print(hasattr(asyncpg.Connection, '__aenter__'))
-    print(hasattr(asyncpg.pool.PoolAcquireContext, '__aenter__'))
-
-asyncio.run(run())
