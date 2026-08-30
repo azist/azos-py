3255d1699675ca132e0bd53096f95f2fd89eab56
DKh <d@zhabis.com>
2026-08-29 22:35:04 -0400

    Latest

--- toys/ctreet.py
+++ toys/ctreet.py
@@ -4,8 +4,9 @@ from azos.sky.ctree import ConfigTree
 
 app = AppChassis("gov", __file__, descriptor_factory=PgSqlCtreeChassisDescriptorFactory())
 
+print(app.descriptor.data)
+
 app.deps.register(PgConnector, app.make_configured(PgConnector, "pg-connector", default_type_name="PgConnector"))
 app.deps.register(ConfigTree, app.make_specific(ConfigTree, "ctree"))
 
-print(app.descriptor.data)
 
