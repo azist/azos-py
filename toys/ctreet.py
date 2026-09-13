aa5ba23a1d3742b4afd8072a19779a04ebfc0352
DKh <d@zhabis.com>
2026-09-12 21:13:41 -0400

    Logging terse/short visual/full

--- toys/ctreet.py
+++ toys/ctreet.py
@@ -6,7 +6,7 @@ app = AppChassis("gov", __file__, descriptor_factory=PgSqlCtreeChassisDescriptor
 
 print(app.descriptor.data)
 
-app.deps.register(PgConnector, app.make_configured(PgConnector, "pg-connector", default_type_name="PgConnector"))
-app.deps.register(ConfigTree, app.make_specific(ConfigTree, "ctree"))
+#app.deps.register(PgConnector, app.make_configured(PgConnector, "pg-connector", default_type_name="PgConnector"))
+#app.deps.register(ConfigTree, app.make_specific(ConfigTree, "ctree"))
 
 
